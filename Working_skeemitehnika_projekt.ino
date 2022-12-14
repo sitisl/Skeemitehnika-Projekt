91f741bf20f737ec46775330fda7dceba0de4034
sitisl <120010042+sitisl@users.noreply.github.com>
2022-12-14 09:05:12 +0200

    Update Working_skeemitehnika_projekt.ino

--- Working_skeemitehnika_projekt.ino
+++ Working_skeemitehnika_projekt.ino
@@ -3,41 +3,63 @@
 #include <LcdBarGraphRobojax.h>
 
 #define READ_PIN A0
+#define MAX_ANALOG_READ 700
+#define MAX_LED_VAL 750
 #define GRAPH_LENGTH 8
+                        //16x2 ekraan
+                        //Ühel real 2 filtrit
+                        //16/2 = 8 veergu ühe filtri jaoks
 
 byte lcdNumCols = 16;
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-LcdBarGraphRobojax filter1(&lcd, GRAPH_LENGTH, 0, 0);
-LcdBarGraphRobojax filter2(&lcd, GRAPH_LENGTH, 0, 1);
-LcdBarGraphRobojax filter3(&lcd, GRAPH_LENGTH, 8, 0);
-LcdBarGraphRobojax filter4(&lcd, GRAPH_LENGTH, 8, 1);
+LiquidCrystal_I2C lcd(0x27,20,2);  // LCD aadress 0x27, 0x20 = 16 veergu, 2 rida 
+
+LcdBarGraphRobojax filterLP(&lcd, GRAPH_LENGTH, 0, 0);      //Low-pass.            Up/left
+
+LcdBarGraphRobojax filterBP_L(&lcd, GRAPH_LENGTH, 8, 0);    //Band-pass low freq.  Up/right
+
+LcdBarGraphRobojax filterBP_H(&lcd, GRAPH_LENGTH, 0, 1);    //Band-pass high freq. Down/right
+
+LcdBarGraphRobojax filterHP(&lcd, GRAPH_LENGTH, 8, 1);      //High-pass            Down/left
 void setup()
 {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   lcd.init();
   lcd.clear();
   delay(50);
   
   lcd.backlight();
-  lcd.print("HALLO");
+  lcd.print("");
   delay(100);
   lcd.clear();
 }
 
+int inputSafe(int a, int b, int c, int d){
+  if(a >= MAX_ANALOG_READ || b >= MAX_ANALOG_READ || c >= MAX_ANALOG_READ || d >= MAX_ANALOG_READ){
+    digitalWrite(LED_BUILTIN, HIGH);
+    lcd.setCursor(0,0);
+    lcd.clear();
+    lcd.print("Lower volume");
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    lcd.clear();
+    return 0;
+  }
+  return 1;
+}
 
-
-
-
-
-void loop()
-{
-  int reading = analogRead(A0);
-  int reading2 = analogRead(A1);
-  int reading3 = analogRead(A2);
-  int reading4 = analogRead(A3);
-  filter1.drawValue(reading, 750);
-  filter2.drawValue(reading2,750);
-  filter3.drawValue(reading3,750);
-  filter4.drawValue(reading4,750);
+void loop(){
+  
+  int LP = analogRead(A2);
+  int BP_L = analogRead(A3);
+  int HP = analogRead(A1);
+  int BP_H = analogRead(A0);
+  
+  if(inputSafe(LP, BP_L, HP, BP_H)){
+    filterLP.drawValue(LP, MAX_LED_VAL);
+    filterHP.drawValue(HP,MAX_LED_VAL);
+    filterBP_L.drawValue(BP_L,MAX_LED_VAL);
+    filterBP_H.drawValue(BP_H,MAX_LED_VAL);
+   }
 }
